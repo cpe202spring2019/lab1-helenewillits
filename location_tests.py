99c4aec5ee660f80d5e2c079eac6b92e1256f789
Helene Willits <49109622+helenewillits@users.noreply.github.com>
2019-04-10 10:14:22 -0700

    Update location_tests.py

--- location_tests.py
+++ location_tests.py
@@ -1,13 +1,26 @@
 import unittest
 from location import *
 
+
 class TestLab1(unittest.TestCase):
 
     def test_repr(self):
+        loc = None
+        self.assertEqual(repr(loc), 'None')  # checks that a location with no information returns None
+
+        # checks several locations
         loc = Location("SLO", 35.3, -120.7)
-        self.assertEqual(repr(loc),"Location('SLO', 35.3, -120.7)")
-    
-    # Add more tests!
+        self.assertEqual(repr(loc), "Location('SLO', 35.3, -120.7)")
+        loc = Location("Los Angelos", 34.052, -118.243)
+        self.assertEqual(repr(loc), "Location('Los Angelos', 34.052, -118.243)")
+        loc = Location("Paris", 48.9, 2.4)
+        self.assertEqual(repr(loc), "Location('Paris', 48.9, 2.4)")
+
+        # tests a change in reference
+        loc1 = loc
+        self.assertEqual(repr(loc1), repr(loc))
+        self.assertEqual(repr(loc1), "Location('Paris', 48.9, 2.4)")
+
 
 if __name__ == "__main__":
-        unittest.main()
+    unittest.main()
