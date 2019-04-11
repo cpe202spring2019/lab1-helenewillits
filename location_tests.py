1262a0d05b03d943408b2d13c261db5742cea07c
Helene Willits <49109622+helenewillits@users.noreply.github.com>
2019-04-10 17:48:42 -0700

    Update location_tests.py

--- location_tests.py
+++ location_tests.py
@@ -10,16 +10,16 @@ class TestLab1(unittest.TestCase):
 
         # checks several locations
         loc = Location("SLO", 35.3, -120.7)
-        self.assertEqual(repr(loc), "Location('SLO', 35.3, -120.7)")
+        self.assertEqual(repr(loc), 'SLO, Latitude:35.3, Longitude:-120.7')
         loc = Location("Los Angelos", 34.052, -118.243)
-        self.assertEqual(repr(loc), "Location('Los Angelos', 34.052, -118.243)")
+        self.assertEqual(repr(loc), 'Los Angelos, Latitude:34.052, Longitude:-118.243')
         loc = Location("Paris", 48.9, 2.4)
-        self.assertEqual(repr(loc), "Location('Paris', 48.9, 2.4)")
+        self.assertEqual(repr(loc), 'Paris, Latitude:48.9, Longitude:2.4')
 
         # tests a change in reference
         loc1 = loc
         self.assertEqual(repr(loc1), repr(loc))
-        self.assertEqual(repr(loc1), "Location('Paris', 48.9, 2.4)")
+        self.assertEqual(repr(loc1), 'Paris, Latitude:48.9, Longitude:2.4')
 
 
 if __name__ == "__main__":
